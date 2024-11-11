0cb662d786d44ce83e2e48443f602ac77ef32c6b
Maria Hernandez <herna835@csusm.edu>
2024-11-10 19:47:00 -0800

    secondBudgetCommit

--- Code/Budget.java
+++ Code/Budget.java
@@ -9,9 +9,18 @@ public class Budget {
         String category;
         String frequency;
         double amount;
-        Scanner scanner = new scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         boolean setNotification;
         boolean validInput = false;
+
+        //First the user must input a category
+        System.out.print("Enter category:");
+        category = scanner.nextLine();
+
+        //User inputs budget amount
+        System.out.print("Enter the amount you would like to spend");
+        String inputUserAmount = scanner.nextLine();
+        amount = Double.parseDouble(inputUserAmount);
 
 
 
