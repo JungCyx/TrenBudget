c7fbf532711d555b33bf3951ec5c0a7cbcd32b30
Maria Hernandez <herna835@csusm.edu>
2024-11-10 19:41:10 -0800

    firstBudgetCommit

---
+++ Code/Budget.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Budget {
+
+    //Method to create a new budget
+    public void createBudget(){
+
+        String category;
+        String frequency;
+        double amount;
+        Scanner scanner = new scanner(System.in);
+        boolean setNotification;
+        boolean validInput = false;
+
+
+
+
+    }
+    
+}
