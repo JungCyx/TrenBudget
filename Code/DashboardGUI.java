274c218d1c697e4f511be991a8a96b6d528a52a3
TariqElamin <tariq.baha558@gmail.com>
2024-11-11 18:08:28 -0800

    Add Dashboard page

---
+++ Code/DashboardGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class DashboardGUI extends JPanel {
+    public DashboardGUI() {
+        setBorder(BorderFactory.createTitledBorder("Dashboard Page"));
+        setSize(400, 500);
+
+
+    }
+}
---
+++ Code/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Create and display the login GUI
+       MainFream mainFream = new MainFream();
+
+    }
+}
